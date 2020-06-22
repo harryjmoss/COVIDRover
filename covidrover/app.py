bda707de0fca883571845d5ddae2d56cd428d39a
harryjmoss <harryjamesmoss1@gmail.com>
2020-06-22 03:53:36 +0100

    More updates and restructuring. Setting up 24h cron job to update plots.

--- covidrover/app.py
+++ covidrover/app.py
@@ -3,21 +3,9 @@ from flask_bootstrap import Bootstrap
 from flask_appconfig import AppConfig
 from flask import Flask, request, render_template
 
-def main(output_files):
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+    AppConfig(app)
+    return app
 
-    def create_app():
-        app = Flask(__name__)
-        Bootstrap(app)
-        AppConfig(app)
-        return app
-
-    app =create_app()
-
-    @app.route('/')
-    def index():
-        return render_template("index.html")
-
-    def run():
-        app.run(debug=True, use_reloader=True)
-
-    run()
--- runapp.py
+++ runapp.py
@@ -1,13 +1,37 @@
 import os
 import sys
+import atexit
 from covidrover import app
 from covidrover import update_plots
-
+from datetime import datetime
+from flask import Flask, request, render_template
+from apscheduler.schedulers.background import BackgroundScheduler
 sys.path.append(os.path.dirname(__name__))
-# generate plots on the first run of the app...
-#outfile_names=update_plots.main()
 
-outfile_names =""
+def daily_updates():
+    update_plots.main()
+    last_update=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return
 
-# run the app
-app.main(outfile_names)
+# set up daily update as a background job
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=daily_updates, trigger="interval", hours=24)
+scheduler.start()
+
+
+last_update=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+app=app.create_app()
+
+@app.route('/')
+def home():
+    return render_template("index.html",lastUpdate=last_update)
+
+def run():
+    app.run(debug=True, use_reloader=False)
+
+run()
+
+
+atexit.register(lambda: scheduler.shutdown())
